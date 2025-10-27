abf491c756c20a8f6877f94420a7b9447d0c7fdf
Sachin Pal <sachinpalsp1402@gmail.com>
2025-10-27 22:35:08 +0530

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,12 +22,12 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 
 
 server.listen(PORT, () => {
